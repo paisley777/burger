c644699a498f4af2bb6b1cf0d3b83d92e54b3ca2
paisley777 <jthurk@northwestern.edu git config --global user.name Your>
2017-08-20 16:33:24 -0500

    set up dbKey as config variable

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+var dbKey = require("../config/dbKey.js");
+
+var connection = mysql.createConnection({
+  port: 3306,
+  host: "localhost",
+  user: "root",
+  password: process.env.password || dbKey,
+  database: "burgers_db"
+});
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for ORM to use.
+module.exports = connection;
