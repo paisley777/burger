741543828081643b67bb8dd67438d34a66f083fd
paisley777 <jthurk@northwestern.edu git config --global user.name Your>
2017-08-20 14:23:08 -0500

    database setup

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Guacamole Burger');
+INSERT INTO burgers (burger_name) VALUES ('Black Bean Burger');
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Burger');
